b4252dd2307c79e10afc8216e6161f043741ed7d
suhashemanth <suhas@fortuneplastech.com>
2025-03-17 15:54:46 +0530

    added dependecies

---
+++ src/main/java/com/suhas/UserPageVerification/UserPageVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.suhas.UserPageVerification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPageVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPageVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suhas/UserPageVerification/UserPageVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhas.UserPageVerification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPageVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
